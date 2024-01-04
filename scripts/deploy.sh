7cad95c4292606123121750d209867c6fdbb2533
manchott <manchott@naver.com>
2024-01-04 11:31:02 +0900

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
+JAR_PATH=/home/ec2-user/app/table-top-tracker/TTT-server
 ROOT=/home/ec2-user/app
+DEPLOY_LOG_PATH=/home/ec2-user/app/deploy.log
 
-echo "> Build 파일 복사"
+echo "> Build 파일 복사" >> $DEPLOY_LOG_PATH
 
-cp $ROOT/zip/*.jar $ROOT/
-
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+cp $JAR_PATH/*.jar $ROOT/
 
 CURRENT_PID=$(pgrep -fl ttt-springboot-webservice | grep jar | awk '{print $1}')
 
-echo "현재 구동중인 애플리게이션 pid: $CURRENT_PID"
+echo "현재 구동중인 애플리게이션 pid: $CURRENT_PID" >> $DEPLOY_LOG_PATH
 
 if [ -z $CURRENT_PID ]
 then
-     echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+     echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG_PATH
 else
-     echo "> kill -15 $CURRENT_PID"
+     echo "> kill -15 $CURRENT_PID" >> $DEPLOY_LOG_PATH
      kill -15 $CURRENT_PID
     sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 애플리케이션 배포" >> $DEPLOY_LOG_PATH
 
 JAR_NAME=$(ls -tr $ROOT/*.jar | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
-
-echo "> JAR Name: $JAR_NAME 에 실행권한 추가"
+echo "> JAR Name: $JAR_NAME 에 실행권한 추가" >> $DEPLOY_LOG_PATH
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행" >> $DEPLOY_LOG_PATH
 
 nohup java -jar \
  -Dspring.config.location=optional:/application.yml,/home/ec2-user/app/application-prod.yml,/home/ec2-user/app/application-oauth.yml \
