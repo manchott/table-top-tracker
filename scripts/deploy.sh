8c1134e38f532a5ca3fabc9ee30f743af7e9948c
manchott <manchott@naver.com>
2024-01-09 17:11:37 +0900

    Update CI/CD codes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,35 +4,58 @@ JAR_PATH=/home/ec2-user/app/zip/TTT-server/TTT/build/libs
 PROJECT_NAME=TTT
 ROOT=/home/ec2-user/app
 DEPLOY_LOG_PATH=/home/ec2-user/app/deploy.log
+DOCKER_APP_NAME=spring
 
-echo "> Build 파일 복사" >> $DEPLOY_LOG_PATH
+# ROOT으로 이동
+cd $ROOT
 
-cp $JAR_PATH/*.jar $ROOT/
+# 실행중인 blue가 있는지 확인
+# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
-CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+# 배포 시작한 날짜와 시간을 기록
+echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
 
-echo "현재 구동중인 애플리게이션 pid: $CURRENT_PID" >> $DEPLOY_LOG_PATH
+# green이 실행중이면 blue up
+# EXIST_BLUE 변수가 비어있는지 확인
+if [ -z "$EXIST_BLUE" ]
+then 
+     # 로그 파일에 "blue 배포 시작" 추가
+     echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
 
-if [ -z $CURRENT_PID ]
-then
-     echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG_PATH
+     # docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
+	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+     # 30초 동안 대기
+     sleep 30
+
+     # 로그 파일에 "green 중단 시작" 추가
+     echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
+
+     # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
+     sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+
+     # 사용하지 않는 이미지 삭제
+     sudo docker image prune -af
+
+     echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
+
+# blue가 실행중이면 green up
 else
-     echo "> kill -15 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-     kill -15 $CURRENT_PID
-    sleep 5
+	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
+	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+     sleep 30
+
+     echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
+     sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+     sudo docker image prune -af
+
+     echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
+
 fi
+  echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $DEPLOY_LOG_PATH
 
-echo "> 새 애플리케이션 배포" >> $DEPLOY_LOG_PATH
+  echo "===================== 배포 완료 =====================" >> $DEPLOY_LOG_PATH
+  echo >> $DEPLOY_LOG_PATH
 
-JAR_NAME=$(ls -tr $ROOT/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME 에 실행권한 추가" >> $DEPLOY_LOG_PATH
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행" >> $DEPLOY_LOG_PATH
-
-nohup java -jar \
- -Dspring.config.location=optional:/application.yml,/home/ec2-user/app/application-prod.yml,/home/ec2-user/app/application-oauth.yml \
- -Dspring.profiles.active=prod \
- $JAR_NAME > $ROOT/nohup.out 2>&1 &
